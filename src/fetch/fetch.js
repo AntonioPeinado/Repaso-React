b0e1280ea0f1ee065a8ec4c29e7e49947309d3e1
Antonio Peinado <60774350+AntonioPeinado@users.noreply.github.com>
2020-05-14 18:48:44 +0200

    fetch

--- src/fetch/fetch.js
+++ src/fetch/fetch.js
@@ -1,45 +1,55 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const url = "https://thesimpsonsquoteapi.glitch.me/quotes?" ; 
 class ApiFetch extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      simsonItems: [],
+      quotes: []
     };
   }
 
   componentDidMount() {
-    this._getSimsonItems();
+    this._getQuotes();
   }
-  async _getSimsonItems() {
-    const response = await fetch(
-      "https://thesimpsonsquoteapi.glitch.me/quotes?character=Homer%20Simpson",
-      {
-        method: "GET",
-        headers: { Accept: "application/json" }
-      }
-       
-    );
-    
-    const body = await response.json();
-    this.setState({ simsonItems: body });
-   console.log(body);
+  componentDidUpdate(prevProps){
+    if(this.props.character !== prevProps.character){
+      this._getQuotes();
+    }
   }
+  async _getQuotes() {
+    const res = await fetch(url + encodeURIComponent(this.props.character));
+    const body = await res.json();
+    this.setState({ quotes: body });
+     }
         
 
   render() {
     return (
-      <ol>
-        <li>{JSON.stringify(this.state.simsonItems)}</li>
-      </ol>
+      <div>
+        <h1>{this.props.character}</h1>
+        <ul>
+          {this.state.quotes.map((quote)=>{
+            const styles={};
+            if(quote.characterDirection.toLowerCase() === 'right'){
+              styles.transform = 'rotateY (180deg)';
+            }
+            return (<li key={quote.quote}>
+              <img style={styles} src={quote.image} alt=''/>
+              <span> {quote.quote}</span>
+            </li>
+            )
+  })}
+        </ul>
+      </div>
     );
   }
 }
   
 ReactDOM.render(
   <React.StrictMode>
-    <ApiFetch />
+    <ApiFetch character='Homer Sinpsom'/>
   </React.StrictMode>,
   document.getElementById("root")
 );
